7ba4c0bdd4e089943bb9aa346f8e6a2da1decbb4
b241871029-rgb <b241871029@gmail.com>
2025-10-27 17:17:53 -0400

    Add Eclipse workspace files

---
+++ StudentRegistration/default_package/Exam.java
@@ -0,0 +1,18 @@
+package default_package;
+
+public class Exam {
+	    private String studentCode;
+	    private String subjectCode;
+	    private float score;
+
+	    public Exam(String studentCode, String subjectCode, float score) {
+	        this.studentCode = studentCode;
+	        this.subjectCode = subjectCode;
+	        this.score = score;
+	    }
+
+	    public String getStudentCode() { return studentCode; }
+	    public String getSubjectCode() { return subjectCode; }
+	    public float getScore() { return score; }
+	}
+}
